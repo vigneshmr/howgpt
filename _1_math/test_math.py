9131d92d770ecdf71e834a1532596f37bf470f6c
Vignesh Murugesan <mvignesh@outlook.com>
2025-07-20 09:25:00 -0700

    cleanup: math

--- _1_math/test_math.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from _1_math import math
-
-class TestMathFunctions(unittest.TestCase):
-    def test_hello_world(self):
-        self.assertEqual(math.add(1, 1), 2)
-
-if __name__ == '__main__':
-    unittest.main()
